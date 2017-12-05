4d6654cd5d4591e4ca68682a479e2d9b09a2d319
mfaisal <muhammadfaisal9t3@outlook.com>
2017-12-05 11:24:01 +0500

    Data Types

--- test.c
+++ test.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <float.h>
 int main()
 {
-    printf("Hello, PAKISTAN!\n");
-    printf("Hello, World!\n");
+    // printf("Hello, PAKISTAN!\n");
+    // printf("Hello, World!\n"); 
+
+    // printf("size of %ld\n",sizeof(int));
+    printf("SIze float: %ld\n",sizeof(float));
+    printf("FLT_MIN: %f\n",FLT_MIN);
+    printf("FLT_MAX: %f\n",FLT_MAX);
+    printf("FLT_DIG: %d\n",FLT_DIG);
+    
+
     return 0;
 }
